7de335cb05c3ca6be2776594915de3add86516b7
Raghuveer <1025486+iamraghuveer@users.noreply.github.com>
2025-02-25 15:04:57 +0530

    fix code

--- main.tf
+++ main.tf
@@ -33,26 +33,6 @@ module "blog_vpc" {
   }
 }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
-  version = "8.1.0"
-  # insert the 1 required variable here
-
-  name = "blog"
-  min_size = 1
-  max_size = 2
-
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-
-  security_group = [module.blog_sg.security_group_id]
-  
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-}
 
 module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
@@ -86,6 +66,24 @@ module "blog_alb" {
   tags = {
     Environment = "dev"
   }
+}
+
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "8.1.0"
+  # insert the 1 required variable here
+
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
+
+  image_id           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 }
 
 
